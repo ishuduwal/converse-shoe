ead95bffe050a2afaab999cab77611782cca2a26
duwazza <115239975+ishuduwal@users.noreply.github.com>
2023-05-04 21:17:59 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const plusBtn = document.querySelector('.plus-btn');
-const minusBtn = document.querySelector('.minus-btn');
-const numberInput = document.querySelector('.number-input');
-const totalPrice = document.getElementById('total-one');
-let price = parseInt(totalPrice.textContent);
-plusBtn.addEventListener('click', () => {
-  let currentValue = parseInt(numberInput.value);
-  numberInput.value = currentValue + 1;
-  price +=110;
-  totalPrice.textContent = price;
+const plusBtns = document.querySelectorAll('.plus-btn');
+const minusBtns = document.querySelectorAll('.minus-btn');
+const numberInputs = document.querySelectorAll('.number-input');
+const totalPrices = document.querySelectorAll('#total-one');
+let prices = Array.from(totalPrices).map(el => parseInt(el.textContent));
+
+plusBtns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    let currentValue = parseInt(numberInputs[index].value);
+    numberInputs[index].value = currentValue + 1;
+    prices[index] += 110;
+    totalPrices[index].textContent = prices[index];
+    console.log("clicked");
+  });
 });
 
-minusBtn.addEventListener('click', () => {
-  let currentValue = parseInt(numberInput.value);
-  if (currentValue > 1) {
-    numberInput.value = currentValue - 1;
-    price -=110;
-    totalPrice.textContent = price;
-  }
+minusBtns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    let currentValue = parseInt(numberInputs[index].value);
+    if (currentValue > 1) {
+      numberInputs[index].value = currentValue - 1;
+      prices[index] -= 110;
+      totalPrices[index].textContent = prices[index];
+    }
+  });
 });
